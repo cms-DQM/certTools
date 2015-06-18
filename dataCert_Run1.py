18f779f1f9c66d4de312dd5cce2bb3115486ec85
Ringaile Placakyte <ringaile@mail.desy.de>
2015-06-18 10:26:08 +0200

    updated dataCert script for JSON production in Run-2: added access to DAS database, beamPresetn and BeamStable conditions; older version moved to dataCert_Run1.py

--- dataCert_Run1.py
+++ dataCert_Run1.py
@@ -4,12 +4,10 @@
 #
 # $Author: smaruyam $
 # $Date: 2012/12/14 17:02:45 $
-# $Revision: 1.15 $ 18.06.2015 $
+# $Revision: 1.14 $
 #
 #
 # Marco Rovere = marco.rovere@cern.ch
-# Laura Borello = Laura.Borrello@cern.ch 
-# Ringaile Placakyte = ringaile@mail.desy.de
 #
 ################################################################################
 
@@ -44,10 +42,6 @@ class Certifier():
         self.address = CONFIG.get('Common','RUNREG')
         self.runmin  = CONFIG.get('Common','RUNMIN')
         self.runmax  = CONFIG.get('Common','RUNMAX')
-        self.runlist = "" 
-        for item in cfglist:
-            if "RUNLIST" in item[0].upper():
-                self.runlist = item[1].split(" ")
         self.qflist  = CONFIG.get('Common','QFLAGS').split(',')
 
         self.bfield_thr  = '-0.1'
@@ -60,11 +54,8 @@ class Certifier():
         self.dbs_pds_all = ""
         self.online_cfg  = "FALSE"
         self.usedbs = False
-        self.useDAS = False
         self.dsstate = ""
         self.useDBScache = "False"
-        self.useBeamPresent = "False"
-        self.useBeamStable = "False"
         self.cacheFiles = []
         self.predefinedPD = ["/MinimumBias/Run2012A-v1/RAW","/MinimumBias/Run2012B-v1/RAW","/MinimumBias/Run2012C-v1/RAW","/MinimumBias/Run2012D-v1/RAW"]
         self.component = []
@@ -72,8 +63,6 @@ class Certifier():
 
         print "First run ", self.runmin
         print "Last run ", self.runmax
-        if len(self.runlist)>0:
-            print "List of runs ", self.runlist, " (",len(self.runlist), " runs)"
         print "Dataset name ", self.dataset
         print "Group name ", self.group
         print "Quality flags ", self.qflist
@@ -91,20 +80,12 @@ class Certifier():
             if "DBS_PDS" in item[0].upper():
                 self.dbs_pds_all = item[1]
                 self.usedbs = True
-            if "USE_DAS" in item[0].upper():
-                self.useDAS = item[1]
             if "ONLINE" in item[0].upper():
                 self.online_cfg = item[1]
             if "DSSTATE" in item[0].upper():
                 self.dsstate = item[1]
             if "DBSCACHE" in item[0].upper():
                 self.useDBScache = item[1]
-            if "BEAMPRESENT" in item[0].upper():
-                self.useBeamPresent = item[1]
-                print 'Use Beam Present Flag', self.useBeamPresent
-            if "BEAMSTABLE" in item[0].upper():
-                self.useBeamStable = item[1]
-                print 'Use Beam Stable Flag', self.useBeamStable
             if "CACHEFILE" in item[0].upper():
                 self.cacheFiles = item[1].split(',')
             if "COMPONENT" in item[0].upper():
@@ -115,11 +96,6 @@ class Certifier():
                 print 'NoLowPU', self.nolowpu
                         
         self.dbs_pds = self.dbs_pds_all.split(",")
-
-        if self.useDAS == "True":
-            self.usedbs = False
-        print "Using DAS database: ", self.useDAS
-        print "Using Cache? : ", self.useDBScache
 
         self.online = False
         if "TRUE" == self.online_cfg.upper() or \
@@ -178,20 +154,8 @@ class Certifier():
         for dcs in self.dcslist:
             if dcs != "NONE":
                 self.filter.setdefault(dcs.lower()+"Ready", "isNull OR  = true")
-#                self.filter.setdefault(dcs.lower(), "isNull OR  = true")
                 if self.verbose: print dcs
-                print dcs
             
-        if self.useBeamPresent == "True":
-            print "Removing LS with no beam present"
-            self.filter.setdefault("beam1Present", "isNull OR  = true")
-            self.filter.setdefault("beam2Present", "isNull OR  = true")
-
-        if self.useBeamStable == "True":
-            print "Removing LS with non-stable beams"
-            self.filter.setdefault("beam1Stable", "isNull OR  = true")
-            self.filter.setdefault("beam2Stable", "isNull OR  = true")
-
         if self.online:
             self.filter.setdefault("dataset", {})\
                                               .setdefault("filter", {})\
@@ -228,8 +192,7 @@ class Certifier():
                                                    .setdefault("filter", {})\
                                                    .setdefault("run", {})\
                                                    .setdefault("filter",{})\
-                                                   .setdefault(comp.lower()+"Present", " = true")\
-                                                   .setdefault(comp.lower()+"Stable", " = true")
+                                                   .setdefault(comp.lower()+"Present", " = true")
 
         if len(self.dsstate):
             self.filter.setdefault("dataset", {})\
@@ -267,25 +230,8 @@ class Certifier():
         if self.verbose:
             print "Printing JSON file ", json.dumps(self.cert_json)
         self.convertToOldJson()
-
-        dbsjson={}
-        if self.useDBScache == "True":
-            dbsjson=get_cachejson(self, self.dbs_pds_all) 
-        elif self.usedbs:
-            dbsjson=get_dbsjson(self, self.dbs_pds_all, self.runmin, self.runmax, self.runlist)
-        elif self.useDAS:   
-            dbsjson=get_dasjson(self, self.dbs_pds_all, self.runmin, self.runmax, self.runlist)
-        else:
-# special case, e.g. cosmics which do not need DB or cache file
-            print "\nINFO: no cache or DB option was selected in cfg file" 
-           
-        if self.useDBScache == "True" or \
-           self.usedbs or \
-           self.useDAS:
-
-            if len(dbsjson)==0: 
-                print "\nERROR, dbsjson contains no runs, please check!" 
-                sys.exit(1)
+        if self.usedbs:
+            dbsjson=get_dbsjson(self, self.dbs_pds_all, self.runmin, self.runmax)
             if self.verbose:
                 print "Printing dbsjson ", dbsjson
             for element in self.cert_old_json:
@@ -296,7 +242,7 @@ class Certifier():
                     print " debug:  Bad Lumi ", dbsbad_int 
                 for interval in  dbsbad_int:
                     combined.append(interval)
-            
+                
                 if element in dbsjson.keys():
                     if self.verbose:
                         print " debug: Found in DBS, Run ", element, ", Lumi ", dbsjson[element]
@@ -340,10 +286,10 @@ class Certifier():
         js = open(self.jsonfile, 'w+')
         json.dump(self.cert_old_json, js, sort_keys=True)
         js.close()
-#       print json file name
-        print " "
-        print "-------------------------------------------"
-        print "Json file: %s written.\n" % self.jsonfile
+        if self.verbose:
+            print " "
+            print "-------------------------------------------"
+            print "Json file: %s written.\n" % self.jsonfile
         
 
 def invert_intervals(intervals,min_val=1,max_val=9999):
@@ -399,102 +345,40 @@ def merge_intervals2(intervals):
     result.append((a, b))
     return result
 
-
-def get_cachejson(self, datasets):
+def get_dbsjson(self, datasets, runmin, runmax):
     unsorted={}
-    lumirangesjson=[]
-    lumirangejson={}
-    fileformatjson = False
     for ds in  datasets.split(","):
-        if (ds in self.predefinedPD) :
+        if self.useDBScache == "True" and (ds in self.predefinedPD) :
             for cacheName in self.cacheFiles:
                 cacheFile = open(cacheName)
                 for line in cacheFile:
-# 
-                    if '[' in line:
-#                        print "List in cache file is json format "
-                        fileformatjson = True
-
                     runlumi=line.split()
                     if len(runlumi) > 1:
                         if runlumi[0].isdigit():
                             run=runlumi[0]
-
-                            if fileformatjson:
-                                runlumic=runlumi[1:]
-                                lumirange=[]
-                                for i, v in enumerate(runlumic):
-                                    if not i%2:
-                                        lowlumi=int(v.replace('[','').replace(']','').replace(',','')) 
-                                    else:
-                                        highlumi=int(v.replace('[','').replace(']','').replace(',','')) 
-                                        lumi=range(lowlumi,highlumi+1)
-                                        lumirange+=lumi
-                                if run not in unsorted.keys():
-                                    unsorted[run]=[]
-                                    unsorted[run]=lumirange
-
-			    else:
-                                if run not in unsorted.keys():
-                                    unsorted[run]=[]
-                                    for lumi in runlumi[1:]:
-                                        unsorted[run].append(int(lumi))
+                            if run not in unsorted.keys():
+                                unsorted[run]=[]
+                            for lumi in runlumi[1:]:
+                                unsorted[run].append(int(lumi))
                 cacheFile.close()
-  
-    sorted={}
-    for run in unsorted.keys():
-       lumilist=unsorted[run]
-       lumilist.sort()
-       sorted[run]=lumilist
-
-    dbsjson={}
-    for run in sorted.keys():
-       lumilist=sorted[run]
-       lumiranges=[]
-       lumirange=[]
-       lumirange.append(lumilist[0])
-       lastlumi=lumilist[0]
-       for lumi in lumilist[1:]:
-           if lumi>lastlumi+1:
-               lumirange.append(lastlumi)
-               lumiranges.append(lumirange)
-               lumirange=[]
-               lumirange.append(lumi)
-           lastlumi=lumi
-       if len(lumirange)==1:
-           lumirange.append(lastlumi)
-           lumiranges.append(lumirange)
-       dbsjson[run]=lumiranges
-
-    return dbsjson
-
-def get_dbsjson(self, datasets, runmin, runmax, runlist):
-    unsorted={}
-    for ds in  datasets.split(","):
-
-        command='' 
-        if len(runlist)>0:
-           command='dbs search --query="find run,lumi where dataset=%s and run in (%s)"' % (ds, runlist)
-           print "\nWARNING: dbs seach will only work if RunList contains less then 650 runs (this option to become obsolete!)" 
-        else: 
-           command='dbs search --query="find run,lumi where dataset=%s and run >=%s  and run<=%s"' % (ds, runmin, runmax)
-        print command
-
-        (status, out) = commands.getstatusoutput(command)
-        if status: 
-            sys.stderr.write(out)
-            print "\nERROR on dbs command: %s\nHave you done cmsenv?" % command
-            sys.exit(1)
-        for line in out.split('\n'):
-            fields=line.split()
-            if len(fields)!=2:
-                continue
-            if fields[0].isdigit() and fields[1].isdigit():
-                run=fields[0]
-                lumi=int(fields[1])
-                if run not in unsorted.keys():
-                     unsorted[run]=[]
-                unsorted[run].append(lumi)
+        else:
+            command='dbs search --query="find run,lumi where dataset=%s and run >=%s  and run<=%s"' % (ds, runmin, runmax)
+            print command
+            (status, out) = commands.getstatusoutput(command)
+            if status: 
+                sys.stderr.write(out)
+                print "\nERROR on dbs command: %s\nHave you done cmsenv?" % command
+                sys.exit(1)
+            for line in out.split('\n'):
+                fields=line.split()
+                if len(fields)!=2:
+                    continue
+                if fields[0].isdigit() and fields[1].isdigit():
+                    run=fields[0]
+                    lumi=int(fields[1])
+                    if run not in unsorted.keys():
+                        unsorted[run]=[]
+                    unsorted[run].append(lumi)
 
     sorted={}
     for run in unsorted.keys():
@@ -522,57 +406,6 @@ def get_dbsjson(self, datasets, runmin, runmax, runlist):
         dbsjson[run]=lumiranges
 
     return dbsjson
-
-
-def get_dasjson(self, datasets, runmin, runmax, runlist):
-    unsorted={}
-    for ds in  datasets.split(","):
-        for runnm in range(int(runmin), int(runmax)+1):
-            foundrun = False
-            if len(runlist)>0:
-#                 if str(runnm) in runlist: 
-#                 print ">>> run in list  = ", runnm
-                for runl in runlist:
-                    if runl.startswith('"'):
-                        runl = runl[1:]
-                    if runl.endswith('"'):
-                        runl = runl[:-1]
-                    if str(runnm) in runl: 
-                        foundrun = True
-            else:
-                foundrun = True
-
-            if foundrun:
-                command='./das_client.py --query="lumi,run dataset=%s run=%s system=dbs3" --format=json --das-headers --limit=0' % (ds, runnm)
-
-                print command
-                (status, out) = commands.getstatusoutput(command)
-
-                if status: 
-                    sys.stderr.write(out)
-                    print "\nERROR on das command: %s\nHave you done cmsenv?" % command
-                    sys.exit(1)
-
-                js=json.loads(out)
-#               print "JSON FORMAT", js
-                try:
-                    js['data'][0]['run'][0]['run_number']
-                except:
-                    continue
-
-                run = js['data'][0]['run'][0]['run_number']
-                lumi = js['data'][0]['lumi'][0]['number']
-
-                if run not in unsorted.keys():
-                   unsorted[run]=[]
-                   for l in lumi:
-	               unsorted[run].append(l)
-
-            dasjson={}
-            for run in unsorted.keys():
-	        dasjson[str(run)]=unsorted[run]
-
-    return dasjson
 
 if __name__ == '__main__':
     cert = Certifier(sys.argv, verbose=False)
