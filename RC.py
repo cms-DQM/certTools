304b7d0b6e8e82c943c00c5f0051b5831d2a2e93
Antanas Norkus <antanas.norkus@cern.ch>
2018-05-11 18:02:11 +0200

    Moved script execution to main method Added global map of workspaces and their DB column names Cleaned-up getRR method to be more descriptive and have clear variable names

--- RC.py
+++ RC.py
@@ -20,64 +20,22 @@ from rhapi import RhApi
 from datetime import date, timedelta
 
 
-#parser = OptionParser()
-parser = argparse.ArgumentParser(description='Make weekly certification HTML for run cordination')
-
-parser.add_argument("-m", "--min",
-        dest="min", type=int, default=314472, help="Minimum run")
-parser.add_argument("-M", "--max",
-        dest="max", type=int, default=999999, help="Maximum run")
-parser.add_argument("-v", "--verbose",
-        dest="verbose", action="store_true", default=False, help="Print more info")
-parser.add_argument("-n", "--notes",
-        dest="notes", type=str, default="notes.txt", help="Text file with notes")
-parser.add_argument("-g", "--group",
-        dest="group", type=str, default="Collisions18", help="Text file with run list")
-parser.add_argument("-a", "--allrun",
-        dest="allrun", action="store_true", default=True, help="Show all runs in the table")
-
-options = parser.parse_args()
-
-if options.verbose:
-    log_level = logging.DEBUG
-else:
-    log_level = logging.INFO
-
-logging.basicConfig(format='[%(levelname)s] [%(asctime)s] %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S', level=log_level)
-
-#TO-DO:wtf if default is true
-logging.debug("verbose:%s" % (options.verbose))
-options.verbose = False
-
-groupName = options.group
-
-runsel = ""
-runsel = '>= %d and <= %d' % (options.min,options.max)
-
-html = ""
-runlist = {}
-
-# List of DET/POG and RR workspace name correspondence
-DetWS = {'PIX': 'Tracker', 'STRIP': 'Tracker', 'ECAL': 'Ecal', 'ES': 'Ecal', \
-        'HCAL': 'Hcal', 'CSC': 'Csc', 'DT': 'Dt', 'RPC': 'Rpc', 'TRACK': 'Tracker', \
-        'MUON': 'Muon', 'JETMET': 'Jetmet', 'EGAMMA': 'Egamma', \
-        'HLT': 'Hlt', 'L1tmu': 'L1t', 'L1tcalo': 'L1t', 'LUMI': 'Lumi'}#, 'CTPPS': 'ctpps'}
-
-#URL = 'http://runregistry.web.cern.ch/runregistry/'
-#api = RRApi(URL, debug=True)
-new_url = "http://vocms00170:2113"
-api = RhApi(new_url, debug=False)
-
-def get_bfield_events(whichRR, dataName, dsState, detPOG):
-    global groupName, runlist
+def get_bfield_events(workspace, datasetName, runClass):
+    """
+    get list of runs from specified start time containing bfield, events for them
+    some doc string here
+    workspace: RunRegistry workspace name
+    datasetName: Dataset name which we look for #TO-DO: do we need it?
+    runClass: type of run Collisions18, Cosmics etc.
+    """
     logging.info("getting bfield and events for runs")
 
+    runlist = {}
     firstDay = str(date.today() - timedelta(days=10))
     __query = ("select r.runnumber, r.bfield, r.events "
             "from runreg_global.runs r where r.run_class_name like '%%%s%%' "
             "and r.starttime >= to_date('%s','yyyy-MM-dd')") % (
-                    groupName, firstDay)
+                    runClass, firstDay)
 
     logging.debug("query to RR: %s" % (__query))
     rr_data2 = api.json(__query)
@@ -93,13 +51,13 @@ def get_bfield_events(whichRR, dataName, dsState, detPOG):
         ##Get run#
         bfield = -1
 
-        run = str(el[0])
+        run = int(el[0])
         bfield = el[1]
         events = el[2]
 
         if run not in runlist:
             runlist[run] = {'B': bfield}
-        if whichRR == 'GLOBAL' and dataName == 'Online':
+        if workspace == 'GLOBAL' and datasetName == 'Online':
             runlist[run]['RR_bfield'] = float(bfield)
             runlist[run]['RR_events'] = int(events)
 
@@ -109,72 +67,53 @@ def get_bfield_events(whichRR, dataName, dsState, detPOG):
 
     logging.debug("list of runs: %s" % (runs))
     logging.debug("runlist: %s" % (json.dumps(runlist)))
+    return runlist
 
-def getRR(whichRR, dataName, dsState, detPOG):
-    global groupName, runreg, runlist, options, runsel
-#    if options.verbose: print "Date of yesterday", str(date.today() - timedelta(days=1))
+def getRR(min_run, datasetName, workspace, datasetClass):
+    """
+    method to query RR with specified input:
+    minimum run we start from
+    dataset_name: Online, Express etc
+    workspace: workspace name
+    datasetClass: dataset class name Collisions18
+    """
+    logging.info("getRRParams: %s %s %s %s" % (min_run, datasetName, workspace, datasetClass))
+
     firstDay = str(date.today() - timedelta(days=10))
-# test ---     firstDay = str(date.today() - timedelta(days=200))
-    ##TO-DO: rewrite to logging
-    #sys.stderr.write("Querying %s RunRegistry for %s dataset\n" % (whichRR, dataName));
-    if whichRR == "GLOBAL":
+    logging.debug("firstDay: %s" % (firstDay))
+
+    if workspace.upper() == "GLOBAL":
         mycolumns = ['run_number', 'rda_state']
     else:
         mycolumns = ['rda_wor_name' , 'run_number', 'rda_state']
-    if options.verbose:
-        print mycolumns
-    text = ''
-    fname = "RR_%s.%s.xml" % (detPOG, groupName)
-    if options.verbose:
-        print "Writing RR information in file %s" % fname
-##Query RR
-    # if api.app == "user":
-    #     print "Checking user RR"
 
-    #     text = api.data(workspace=whichRR.upper(), table='datasets', template='xml',
-    #             columns=mycolumns, filter={'runNumber': '%s' % runsel,
-    #                                         'runClassName':"like '%%%s%%'" % groupName,
-    #                                         'datasetName': "like '%%%s%%'" % dataName},
-    #             tag='LATEST')
+    text = ''
+    fname = "RR_%s.%s_new.json" % (workspace, datasetClass)
+    logging.debug("mycolumns: %s" % (mycolumns))
+    logging.debug("Writing RR information in file %s" % (fname))
 
     ##TO-DO: hope the table alias is 'r' all the time...
     __sql_columns = []
+
     for el in mycolumns:
         __sql_columns.append("r.%s" % (el))
 
     logging.debug("%s %s %s %s %s" % (
-                ",".join(__sql_columns), whichRR.lower(),
-                options.min, groupName, dataName))
+                ",".join(__sql_columns), workspace.lower(),
+                min_run, datasetClass, datasetName))
 
     __query = ("select %s from runreg_%s.datasets r "
         "where r.run_number >= %s and r.run_class_name like '%%%s%%' "
         "and r.rda_name like '%%%s%%'") % (
-                ",".join(__sql_columns), whichRR.lower(),
-                options.min, groupName, dataName)
+                ",".join(__sql_columns), workspace.lower(),
+                options.min, datasetClass, datasetName)
 
-    rr_data = api.xml(__query)
+    logging.debug("RR query: %s" % (__query))
+
+    rr_data = api.json(__query)
     ##write xml output to file
-    log = open(fname,"w");
-    log.write(rr_data); log.close()
-    ##Get and Loop over xml data
-    dom = ''
-    domP = None
-
-    try:
-        dom  = parseString(rr_data)
-    except:
-        ##In case of a non-Standard RR output (dom not set)
-        print "Could not parse RR output"
-
-# Getting info on the Bfield and the run taken in the last 10 days
-#    if whichRR == "GLOBAL":
-        # text_bfield = api.data(workspace='GLOBAL', table='runsummary', template='xml',
-        #         columns=['number','bfield','events'], filter={
-        #             "runClassName": "like '%%%s%%'"%groupName,
-        #             "startTime": ">= %s" % firstDay,
-        #             "datasets": {"rowClass": "org.cern.cms.dqm.runregistry.user.model.RunDatasetRowGlobal"}}
-        #         )
-
+    with open(fname,"w") as out_file:
+        out_file.write(json.dumps(rr_data, indent=4))
 
 def getCertification(det, run, runlistdet):
     global groupName, runreg, runlist, options, runsel
@@ -210,6 +149,7 @@ def getCertification(det, run, runlistdet):
             print "STATE", state
             isopen = (state  == "OPEN")
             lumis = 0
+            ##TO-DO:get a global dictionary with workspace and the columns to be displayed
             status = mydata.getElementsByTagName(mycolumns[0])[0].getElementsByTagName('status')[0].firstChild.data
             if options.verbose:
                 print status
@@ -243,8 +183,89 @@ def p2t(pair):
     else:
         return verdict
 
+if __name__ == '__main__':
+    #parser = OptionParser()
+    parser = argparse.ArgumentParser(description='Make weekly certification HTML for run cordination')
+    parser.add_argument("-m", "--min",
+            dest="min", type=int, default=314472, help="Minimum run")
+    parser.add_argument("-M", "--max",
+            dest="max", type=int, default=999999, help="Maximum run")
+    parser.add_argument("-v", "--verbose",
+            dest="verbose", action="store_true", default=False, help="Print more info")
+    parser.add_argument("-n", "--notes",
+            dest="notes", type=str, default="notes.txt", help="Text file with notes")
+    parser.add_argument("-g", "--group",
+            dest="group", type=str, default="Collisions18", help="Text file with run list")
+    parser.add_argument("-a", "--allrun",
+            dest="allrun", action="store_true", default=True, help="Show all runs in the table")
 
-html = """
+    options = parser.parse_args()
+
+    if options.verbose:
+        log_level = logging.DEBUG
+    else:
+        log_level = logging.INFO
+
+    logging.basicConfig(format='[%(asctime)s] [%(levelname)s] %(message)s',
+            datefmt='%Y-%m-%d %H:%M:%S', level=log_level)
+
+    #TO-DO:wtf if default is true
+    logging.debug("verbose:%s" % (options.verbose))
+    options.verbose = False
+
+    groupName = options.group
+
+    #runsel = ""
+    #runsel = '>= %d and <= %d' % (options.min,options.max)
+
+    runlist = {}
+
+    # List of DET/POG and RR workspace name correspondence
+    # DetWS = {'PIX': 'Tracker', 'STRIP': 'Tracker', 'ECAL': 'Ecal', 'ES': 'Ecal', \
+    #         'HCAL': 'Hcal', 'CSC': 'Csc', 'DT': 'Dt', 'RPC': 'Rpc', 'TRACK': 'Tracker', \
+    #         'MUON': 'Muon', 'JETMET': 'Jetmet', 'EGAMMA': 'Egamma', \
+    #         'HLT': 'Hlt', 'L1tmu': 'L1t', 'L1tcalo': 'L1t', 'LUMI': 'Lumi'}#, 'CTPPS': 'ctpps'}
+
+    ## Definition of global map of workspaces and their representative columns in RR DB
+    # TO-DO: add global workspace and its columns
+    # TO-DO: add multiple columns for comment, cause etc.
+    map_DB_to_column = {"CSC": {"workspace": "CSC",
+                                "DB_column": "RDA_CMP_CSC"},
+                        # "CTPPS": {"workspace": "CTPPS",
+                        #         "DB_column": "RDA_CMP_RP45_210"},  #CTPPS has 6 columns
+                        "DT": {"workspace": "DT",
+                                "DB_column": "RDA_CMP_DT"},
+                        "ECAL": {"workspace": "ECAL",
+                                "DB_column": "RDA_CMP_ECAL"},
+                        "ES": {"workspace": "ECAL",
+                                "DB_column": "RDA_CMP_ES"},
+                        "EGAMMA": {"workspace": "EGAMMA",
+                                "DB_column": "RDA_CMP_EGAMMA"},
+                        "HCAL": {"workspace": "HCAL",
+                                "DB_column": "RDA_CMP_HCAL"}, #this column is not displayed in userRR
+                        "HLT": {"workspace": "HLT",
+                                "DB_column": "RDA_CMP_HLT"},
+                        "JETMET": {"workspace": "JETMET",
+                                "DB_column": "RDA_CMP_JETMET"},
+                        "L1TMU": {"workspace": "L1T",
+                                "DB_column": "RDA_CMP_MUON"},
+                        "L1TCALO": {"workspace": "L1T",
+                                "DB_column": "RDA_CMP_JET"},
+                        "LUMI": {"workspace": "LUMI",
+                                "DB_column": "RDA_CMP_LUMI"},
+                        "MUO": {"workspace": "MUO",
+                                "DB_column": "RDA_CMP_MUON"},
+                        "RPC": {"workspace": "RPC",
+                                "DB_column": "RDA_CMP_RPC"},
+                        "PIX": {"workspace": "TRACKER",
+                                "DB_column": "RDA_CMP_PIXEL"},
+                        "STRIP": {"workspace": "TRACKER",
+                                "DB_column": "RDA_CMP_STRIP"},
+                        "TRACK": {"workspace": "TRACKER",
+                                "DB_column": "RDA_CMP_TRACKING"},
+    }
+
+    html = """
 <html>
 <head>
 <title>Certification of Collision runs recorded in the last 10 days </BR>
@@ -272,45 +293,56 @@ html = """
 <table>
 """ % (time.ctime(), time.ctime())
 
-#we get list of runs, their bfield and number of events
-get_bfield_events("GLOBAL", "Online", "= OPEN OR = SIGNOFF OR = COMPLETED", 'Global')
-sys.exit(-1)
+    #URL = 'http://runregistry.web.cern.ch/runregistry/'
+    #api = RRApi(URL, debug=True)
+    new_url = "http://vocms00170:2113"
+    new_new_url = "http://vocms0185.cern.ch:2113"
+    api = RhApi(new_url, debug=False)
 
-getRR("GLOBAL", "Online", "= OPEN OR = SIGNOFF OR = COMPLETED", 'Global')
+    #we get list of runs, their bfield and number of events
+    runlist = get_bfield_events("GLOBAL", "Online", groupName)
 
-#current one is below
-listDETPOG = ['HLT','L1tcalo','L1tmu','CSC','DT','RPC','ECAL','ES','HCAL','PIX','STRIP','TRACK']#,'CTPPS']
-listDETPOG = ['CSC','DT','RPC','ECAL']#,'CTPPS']
-for i in range(len(listDETPOG)):
-    print "--------- Checking ", listDETPOG[i]
-    localws = DetWS.get(listDETPOG[i])
-    getRR("%s" % localws, "Express" , "= OPEN OR = SIGNOFF OR = COMPLETED", listDETPOG[i])
 
-print "Done"
+    getRR(options.min, "Online", 'Global', groupName)
+    sys.exit(-1)
 
-#html += "<tr><th>Run</th><th>B-field</th><th>CSC</th><th>DT</th><th>ECAL</th><th>ES</th><th>HCAL</th><th>PIX</th><th>RPC</th><th>STRIP</th><th>TRACKING</th></tr>"
-html += "<tr><th>Run</th><th>B-field</th><th>Events</th><th>HLT</th><th>L1T calo</th><th>L1T muon</th><th>CSC</th><th>DT</th><th>RPC</th><th>ECAL</th><th>ES</th><th>HCAL</th><th>PIX</th><th>STRIP</th><th>TRACKING</th><th>CTPPS</th></tr>"
-runs = runlist.keys()
-runs.sort()
-runs.reverse()
-print "ALL RUNS: " , runs , "\n"
+    #current one is below
+    listDETPOG = ['HLT','L1tcalo','L1tmu','CSC','DT','RPC','ECAL','ES','HCAL','PIX','STRIP','TRACK']#,'CTPPS']
+    listDETPOG = ['CSC','DT','RPC','ECAL']#,'CTPPS']
+    sorted_list_of_POGS = ['CSC','CTPPS','DT','ECAL','ES','EGAMMA','HCAL','HLT','JETMET','L1tmu','L1tcalo','LUMI','MUON','RPC','PIX','STRIP','TRACK']
+    # for now ignore CTPPS
+    # TO-DO: add ctpps once we conclude that new API works
+    # TO-DO: RR api add non mandatory variable for needed columns! Status,cause,comment
+    sorted_list_of_POGS2 = ['CSC','DT','ECAL','ES','EGAMMA','HCAL','HLT','JETMET','L1tmu','L1tcalo','LUMI','MUON','RPC','PIX','STRIP','TRACK']
+    for pog in sorted_list_of_POGS2[0]:
+        logging.info("Cheking %s worspace for Express runs" % (pog))
+        getRR("%s" % pog, "Express", pog)
+    logging.info("Finished checking for express runs")
+    sys.exit(-1)
 
-for r in runs:
-    R = runlist[r]
-    html += "<tr><th>%d</th><td class='num'>%.1f T</td><td class='num'>%d</td></td>" % (r, runlist[r]['RR_bfield'], runlist[r]['RR_events'])
-    All_comments = ''
-    for i in range(len(listDETPOG)):
-        localws = DetWS.get(listDETPOG[i])
-        cert = ([False,'WAIT',''])
-        getCertification("%s" % listDETPOG[i], r, cert)
-        print "---- Certification for ", listDETPOG[i] , cert
-        if options.verbose:
-            print "localws", localws
-        html += "<td class='%s'>%s</td>" % (v2c(cert[0], cert[1]), p2t(cert))
-        print v2c(cert[0],cert[1]), p2t(cert)
+    #html += "<tr><th>Run</th><th>B-field</th><th>CSC</th><th>DT</th><th>ECAL</th><th>ES</th><th>HCAL</th><th>PIX</th><th>RPC</th><th>STRIP</th><th>TRACKING</th></tr>"
+    html += "<tr><th>Run</th><th>B-field</th><th>Events</th><th>HLT</th><th>L1T calo</th><th>L1T muon</th><th>CSC</th><th>DT</th><th>RPC</th><th>ECAL</th><th>ES</th><th>HCAL</th><th>PIX</th><th>STRIP</th><th>TRACKING</th><th>CTPPS</th></tr>"
+    runs = runlist.keys()
+    runs.sort()
+    runs.reverse()
+    print "ALL RUNS: " , runs , "\n"
 
-html += "</table></body></html>"
-certday = date.today().strftime("%Y%m%d")
-out = open("status.%s.html" % groupName, "w")
-out.write(html.encode('utf-8'))
-out.close()
+    for r in runs:
+        R = runlist[r]
+        html += "<tr><th>%d</th><td class='num'>%.1f T</td><td class='num'>%d</td></td>" % (r, runlist[r]['RR_bfield'], runlist[r]['RR_events'])
+        All_comments = ''
+        for i in range(len(listDETPOG)):
+            localws = DetWS.get(listDETPOG[i])
+            cert = ([False,'WAIT',''])
+            getCertification("%s" % listDETPOG[i], r, cert)
+            print "---- Certification for ", listDETPOG[i] , cert
+            if options.verbose:
+                print "localws", localws
+            html += "<td class='%s'>%s</td>" % (v2c(cert[0], cert[1]), p2t(cert))
+            print v2c(cert[0],cert[1]), p2t(cert)
+
+    html += "</table></body></html>"
+    certday = date.today().strftime("%Y%m%d")
+    out = open("status.%s.html" % groupName, "w")
+    out.write(html.encode('utf-8'))
+    out.close()
